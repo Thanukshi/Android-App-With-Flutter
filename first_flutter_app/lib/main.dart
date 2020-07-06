0f137cba14feac0cb982c5e59d273f598b110d0c
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-07-06 21:28:07 +0530

    Using Flutter

---
+++ first_flutter_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyFirstFlutterApp());
+}
+
+class MyFirstFlutterApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      //home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App in Flutter'),
+        ),
+      ),
+
+    );
+
+  }
+
+}
